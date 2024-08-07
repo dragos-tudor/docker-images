7f79e4aa24353d9aa9d0f3ef1f497fe42453fb79
Dragos Tudor <dragos.tudor@gmail.com>
2024-08-07 10:53:25 +0300

    update deno image files.

---
+++ publish.deno.sh
@@ -0,0 +1,10 @@
+set -eo pipefail
+IMAGE_VERSION=1.45.2
+
+docker build \
+  --build-arg IMAGE_VERSION=$IMAGE_VERSION \
+  -t ghcr.io/dragos-tudor/deno:$IMAGE_VERSION \
+  -f Dockerfile.deno .
+# echo $GITHUB_TOKEN | docker login ghcr.io -u dragos-tudor --password-stdin
+# docker push ghcr.io/dragos-tudor/deno:$IMAGE_VERSION
+
